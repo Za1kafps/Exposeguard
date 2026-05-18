d7ee23dc9ff93e8510ed9a9fad8fb19d46b65d7f
Za1ka <zuzinzena44@gmail.com>
2026-05-18 04:48:47 +0300

    add exposeguard CLI entrypoint

--- cmd/exposeguard/main.go
+++ cmd/exposeguard/main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/Za1kafps/exposeguard/internal/cli"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("exposeguard: missing command")
-		fmt.Println("usage: exposeguard scan")
-		os.Exit(1)
-	}
-
-	switch os.Args[1] {
-	case "scan":
-		fmt.Println("exposeguard: scan started")
-		fmt.Println("exposeguard: no checks implemented yet")
-	default:
-		fmt.Printf("exposeguard: unknown command %q\n", os.Args[1])
-		os.Exit(1)
-	}
+	os.Exit(cli.Run(os.Args, os.Stdout, os.Stderr))
 }
