3af2ccf6c3690cc4e466f970c1055f68ae711128
Za1ka <zuzinzena44@gmail.com>
2026-06-03 01:25:38 +0300

    add docker fx module

---
+++ internal/docker/module.go
@@ -0,0 +1,25 @@
+package docker
+
+import (
+	"context"
+
+	"github.com/Za1kafps/exposeguard/internal/system"
+	"go.uber.org/fx"
+)
+
+var Module = fx.Module(
+	"docker",
+	fx.Provide(NewDiscovery),
+)
+
+type Discovery struct {
+	runner system.Runner
+}
+
+func NewDiscovery(runner system.Runner) *Discovery {
+	return &Discovery{runner: runner}
+}
+
+func (d *Discovery) Discover(ctx context.Context) Result {
+	return Discover(ctx, d.runner)
+}
